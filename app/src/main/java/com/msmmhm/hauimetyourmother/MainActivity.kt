23fc2a883b26367eef36e5290c2c1a1a845c4bae
sra.mik <srama.mikolaj@gmail.com>
2022-04-29 21:48:31 +0000

    Test

--- app/src/main/java/com/msmmhm/hauimetyourmother/MainActivity.kt
+++ app/src/main/java/com/msmmhm/hauimetyourmother/MainActivity.kt
@@ -3,6 +3,8 @@ package com.msmmhm.hauimetyourmother
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//Test
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
