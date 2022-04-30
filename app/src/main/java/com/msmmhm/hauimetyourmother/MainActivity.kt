11f0437552c1103bceed0e30b2ed9bd43fe872c2
sra.mik <srama.mikolaj@gmail.com>
2022-04-30 12:03:39 +0000

    Firebase registration first attempt

--- app/src/main/java/com/msmmhm/hauimetyourmother/MainActivity.kt
+++ app/src/main/java/com/msmmhm/hauimetyourmother/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 //Test
 
@@ -11,19 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        if(!checkCachedData()){
-            val intent: Intent = Intent(this, LoginAndRegister::class.java)
-            startActivity(intent)
-        }
-        
+
+
+
+
+
     }
     //Check if cached login data exists
-    private fun checkCachedData(): Boolean {
-        val prefs: SharedPreferences = getSharedPreferences("UserData", MODE_PRIVATE)
-        val email: String? = prefs.getString("email", "")
-        val password: String? = prefs.getString("password", "")
-        if(email.equals("") or password.equals("")) return false
-        return true
-    }
+
 }
--- app/src/main/java/com/msmmhm/hauimetyourmother/RegisterFragment.kt
+++ app/src/main/java/com/msmmhm/hauimetyourmother/RegisterFragment.kt
@@ -1,18 +1,25 @@
 package com.msmmhm.hauimetyourmother
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.msmmhm.hauimetyourmother.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
     private lateinit var binding: FragmentRegisterBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
     }
 
     override fun onCreateView(
@@ -28,6 +35,15 @@ class RegisterFragment : Fragment() {
         binding.GoToLoginButton.setOnClickListener{
             findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
+        binding.RegisterButton.setOnClickListener{
+            accountRegister()
+        }
+    }
+
+    private fun accountRegister() {
+        val email = binding.editTextTextPersonName.text.toString()
+        val password = binding.editTextTextPassword.text.toString()
+        auth.createUserWithEmailAndPassword(email, password)
     }
 
     companion object {
