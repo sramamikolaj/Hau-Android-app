409e2a64880084ffaaabe89ebbce4653e9f6eed1
marce <mmroz.marcel@gmail.com>
2022-05-19 16:45:58 +0200

    qr generated and scanned

--- app/src/main/java/com/msmmhm/hauimetyourmother/AddFriendQrScanFragment.kt
+++ app/src/main/java/com/msmmhm/hauimetyourmother/AddFriendQrScanFragment.kt
@@ -5,15 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.msmmhm.hauimetyourmother.databinding.FragmentDashboardBinding
+import android.widget.Toast
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+
 
 class AddFriendQrScanFragment : Fragment() {
     lateinit var binding: AddFriendQrScanFragment
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var codeScanner: CodeScanner
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,10 +21,41 @@ class AddFriendQrScanFragment : Fragment() {
     ): View? {
         //binding = AddFriendQrScanFragment.inflate(layoutInflater)
 
-
-       //return binding.root
+        //return binding.root
         return inflater.inflate(R.layout.fragment_add_friend_qr_scan, container, false)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val scannerView = view.findViewById<CodeScannerView>(R.id.scanner_view)
+        val activity = requireActivity()
+        codeScanner = CodeScanner(activity, scannerView)
+        codeScanner.decodeCallback = DecodeCallback {
+            activity.runOnUiThread {
+                Toast.makeText(activity, it.text, Toast.LENGTH_LONG).show()
+            }
+        }
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+        override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+
+
 
 }
+
+
